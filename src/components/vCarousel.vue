0359c6a1a2ecad10ed6f91eeff50d201c98e5cb6
Kasun <kasun@icanlk.com>
2024-01-05 12:06:49 +0530

    vue carousel

---
+++ src/components/vCarousel.vue
@@ -0,0 +1,20 @@
+<template>
+    <carousel-3d :controls-visible="true" :clickable="true">
+      <slide :index="0">
+        Slide 1 Content
+      </slide>
+      <slide :index="1">
+        Slide 2 Content
+      </slide>
+      <slide :index="2">
+        Slide 3 Content
+      </slide>
+      <!-- Add more slides as needed -->
+    </carousel-3d>
+  </template>
+  
+  <script>
+  export default {
+    // Your component options
+  }
+  </script>
