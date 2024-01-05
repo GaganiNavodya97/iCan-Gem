93984cc65ae6658c34d70134e68eb7807d4ecd01
Kasun <kasun@icanlk.com>
2024-01-05 17:53:44 +0530

    gem carousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
   <main>
 <AppHeader />
+<vCarousel />
   </main>
 </template>
 <script setup>
 import AppHeader from '@/layouts/AppHeader.vue';
+import vCarousel from '@/components/vCarousel.vue';
 
 
 </script>
