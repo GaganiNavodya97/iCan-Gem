550d4c96d3b7ec79f7f278adf2b7f79e14825947
Gagani <lpgagani97@gmail.com>
2024-01-08 09:27:11 +0530

    helps and faqs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="about">
     <news-articles/>
+    <question-help/>
     <app-footer/>
   </div>
 </template>
 <script>
 import AppFooter from '@/layouts/AppFooter.vue';
 import NewsArticles from '@/components/LandingPage/NewsArticles.vue';
+import QuestionHelp from '@/components/LandingPage/QuestionHelp.vue';
 export default {
   components:{
     AppFooter,
-    NewsArticles
+    NewsArticles,
+    QuestionHelp
   },
   name: "AppLayout",
   created() {},
