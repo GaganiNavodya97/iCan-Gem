2569e2da0c0941b798dc9f7629c00bab6d335183
kasunAththanagoda <kasunaththanagoda@gmail.com>
2024-01-09 14:44:29 +0530

    add to cart fonts change

--- src/components/addToCart.vue
+++ src/components/addToCart.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="container mx-auto px-4 bg-[#F6F4F2] py-1">
     <h1 class="text-2xl text-center mt-4 text-[#DFAC93] font-Montserrat">NEW IN</h1>
-    <h1 class="text-5xl text-center my-3 text-[#051F34] font-goudy">New Collection.</h1>
-    <p class="text-center text-lg mb-12 text-[#051F34A8] font-goudy">
+    <h1 class="text-[51.749px] font-normal text-center my-3 text-[#051F34] font-goudy">New Collection.</h1>
+    <p class="text-center text-[27.165px] mb-12 text-[#051F34A8] font-goudy">
       Shine bright in our new 2024 collection of fine jewellery!
     </p>
 
-    <div class="text-center mb-8 mx-10">
+    <div class="text-center mb-8 mx-0">
       <button
         v-for="category in categories"
         :key="category"
-        class="hover:text-[#DFAC93] mx-4 font-Montserrat"
+        class="hover:text-[#DFAC93] mx-3 font-Montserrat leading-10"
         :class="{
           'border-b-2': selectedCategory === category,
           'border-gray-500': selectedCategory === category
@@ -21,7 +21,7 @@
       </button>
     </div>
 
-    <hr />
+    <hr class="-mt-8" />
 
     <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-8 lg:px-8 mt-4">
       <ProductCard v-for="product in filteredProducts" :key="product.id" :product="product" />
