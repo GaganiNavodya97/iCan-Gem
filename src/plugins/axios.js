fe6ea9e9ccf71a37eefa4e82b132b6dfeb6c65f2
kasunAththanagoda <kasunaththanagoda@gmail.com>
2024-01-10 16:24:30 +0530

    added live url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://mall-api.icanim.com', //need to change this to live
+  // baseURL: 'https://mall-api.icanim.com', //need to change this to live
+  baseURL: 'https://service.icanmall.net', //need to change this to live
+
 });
 
 export default instance;
